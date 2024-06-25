8478b20b5adae2478b465a9c51b1f1f317fc18f9
code-sharad <code-sharad>
2024-06-25 12:32:51 +0000

    Updated UI 🎉

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        white: "#fff",
         black: "#000",
+        white: "#fff",
       },
       spacing: {},
       fontFamily: {
